78a0b1e8e3156d2aa894c4c67b8fb711cb0ee51c
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-13 23:40:45 +0530

    added cross type

--- src/types/forms.ts
+++ src/types/forms.ts
@@ -1,8 +1,13 @@
 export type BaseFormType = {
-    name: string,
-    mobile_no: string,
-    unique_code: string,
-    email: string,
-    usn?: string,
-    qrcodedata?: string
-}
+  name: string;
+  mobile_no: string;
+  unique_code: string;
+  email: string;
+  usn?: string;
+  qrcodedata?: string;
+};
+
+export type CrossData = {
+  name: string;
+  time: string;
+};
