76aa1e04140438151d8330ba5962e503da23b005
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-13 12:10:34 +0530

    updated table names and added finish line crossing tables

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,48 +1,63 @@
-import { pgTable, varchar } from "drizzle-orm/pg-core";
+import { pgTable, timestamp, varchar } from "drizzle-orm/pg-core";
 
-export const boys = pgTable('users', {
-    name: varchar('name'),
-    mobileNo: varchar('mobile_no'),
-    uniqueCode: varchar('unique_code').primaryKey(),
-    email: varchar('email'),
-    usn: varchar('usn'),
-    qrcodedata: varchar("qrcodedata")
+export const boys = pgTable("boys", {
+  name: varchar("name"),
+  mobileNo: varchar("mobile_no"),
+  uniqueCode: varchar("unique_code").primaryKey(),
+  email: varchar("email"),
+  usn: varchar("usn"),
+  qrcodedata: varchar("qrcodedata"),
 });
 
-export const girls = pgTable('girls', {
-    name: varchar('name'),
-    mobileNo: varchar('mobile_no'),
-    uniqueCode: varchar('unique_code').primaryKey(),
-    email: varchar('email'),
-    usn: varchar('usn'),
-    qrcodedata: varchar("qrcodedata")
+export const girls = pgTable("girls", {
+  name: varchar("name"),
+  mobileNo: varchar("mobile_no"),
+  uniqueCode: varchar("unique_code").primaryKey(),
+  email: varchar("email"),
+  usn: varchar("usn"),
+  qrcodedata: varchar("qrcodedata"),
 });
 
-export const walkathon = pgTable('walk', {
-    name: varchar('name'),
-    mobileNo: varchar('mobile_no'),
-    uniqueCode: varchar('unique_code').primaryKey(),
-    email: varchar('email'),
-    qrcodedata: varchar("qrcodedata")
+export const walkathon = pgTable("walkathon", {
+  name: varchar("name"),
+  mobileNo: varchar("mobile_no"),
+  uniqueCode: varchar("unique_code").primaryKey(),
+  email: varchar("email"),
+  qrcodedata: varchar("qrcodedata"),
 });
 
-
-export const sit = pgTable('sit', {
-    name: varchar('name'),
-    mobileNo: varchar('mobile_no'),
-    uniqueCode: varchar('unique_code').primaryKey(),
-    email: varchar('email'),
-    usn: varchar('usn'),
-    qrcodedata: varchar("qrcodedata")
+export const sit = pgTable("sit_participants", {
+  name: varchar("name"),
+  mobileNo: varchar("mobile_no"),
+  uniqueCode: varchar("unique_code").primaryKey(),
+  email: varchar("email"),
+  usn: varchar("usn"),
+  qrcodedata: varchar("qrcodedata"),
 });
 
-export const master = pgTable("master",
-    {
-        name: varchar('name'),
-        mobileNo: varchar('mobile_no'),
-        uniqueCode: varchar('unique_code').primaryKey(),
-        email: varchar('email'),
-        usn: varchar('usn'),
-        qrcodedata: varchar("qrcodedata")
-    }
-)
+export const master = pgTable("master", {
+  name: varchar("name"),
+  mobileNo: varchar("mobile_no"),
+  uniqueCode: varchar("unique_code").primaryKey(),
+  email: varchar("email"),
+  usn: varchar("usn"),
+  qrcodedata: varchar("qrcodedata"),
+});
+
+export const boysCross = pgTable("cross_boys", {
+  name: varchar("name"),
+  uniqueCode: varchar("unique_code").primaryKey(),
+  time: timestamp("time").notNull(),
+});
+
+export const girlsCross = pgTable("cross_girls", {
+  name: varchar("name"),
+  uniqueCode: varchar("unique_code").primaryKey(),
+  time: timestamp("time").notNull(),
+});
+
+export const walkathonCross = pgTable("cross_walkathon", {
+  name: varchar("name"),
+  uniqueCode: varchar("unique_code").primaryKey(),
+  time: timestamp("time").notNull(),
+});
