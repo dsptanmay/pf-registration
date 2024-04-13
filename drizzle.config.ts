6ab1d9e6ab1bba6d7310bada2c6b861303c6fb9a
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-13 12:57:53 +0530

    fixed drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "src/database/schema",
+  schema: "src/database/schema.ts",
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
