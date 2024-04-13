c36d4aa767497df0009ca203a15bd1e92caeadd5
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-13 12:14:20 +0530

    added functionality to get top 20/10 participants depending on category

--- src/actions/dataActions.ts
+++ src/actions/dataActions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { BaseFormType } from "@/types/forms"
+import { BaseFormType } from "@/types/forms";
 import { db } from "@/database/db";
 
 import QRCode from "qrcode";
@@ -8,87 +8,117 @@ import * as fs from "fs";
 
 import { getMailOpts, transporter } from "@/actions/mailActions";
 import { eq } from "drizzle-orm";
-import { boys, girls, master, sit, walkathon } from "@/database/schema";
+import {
+  boys,
+  boysCross,
+  girls,
+  girlsCross,
+  master,
+  sit,
+  walkathon,
+  walkathonCross,
+} from "@/database/schema";
 
 export async function getQRCode(uniqueCode: string) {
-    return await db.query.master.findFirst({
-        where: eq(master.uniqueCode, uniqueCode)
-    })
+  return await db.query.master.findFirst({
+    where: eq(master.uniqueCode, uniqueCode),
+  });
 }
 
-export async function pushData(formData: BaseFormType, category: "boys" | "girls" | "walkathon") {
-    const qrCodePath = `qrCode_${Date.now()}.png`;
-    const baseQrData = `name: ${formData.name} uc: ${formData.unique_code}`;
-    const mailOpts = getMailOpts(formData.name, formData.email, qrCodePath);
+export async function getTopParticipants(category: "boys" | "girls" | "walkathon") {
+  if (category === "boys")
+    return await db
+      .select({ name: boysCross.name, time: boysCross.time })
+      .from(boysCross)
+      .orderBy(boysCross.time)
+      .limit(20);
+  else if (category === "girls")
+    return await db
+      .select({ name: girlsCross.name, time: girlsCross.time })
+      .from(girlsCross)
+      .orderBy(girlsCross.time)
+      .limit(20);
+  else if (category === "walkathon")
+    return await db
+      .select({ name: walkathonCross.name, time: walkathonCross.time })
+      .from(walkathonCross)
+      .orderBy(walkathonCross.time)
+      .limit(10);
+}
 
-    let qrData = baseQrData;
-    if (category === 'boys')
-        qrData += " b";
-    else if (category === "girls")
-        qrData += " g";
-    else if (category === "walkathon")
-        qrData += " w";
+export async function pushData(
+  formData: BaseFormType,
+  category: "boys" | "girls" | "walkathon"
+) {
+  const qrCodePath = `qrCode_${Date.now()}.png`;
+  const baseQrData = `name: ${formData.name} uc: ${formData.unique_code}`;
+  const mailOpts = getMailOpts(formData.name, formData.email, qrCodePath);
 
+  let qrData = baseQrData;
+  if (category === "boys") qrData += " b";
+  else if (category === "girls") qrData += " g";
+  else if (category === "walkathon") qrData += " w";
 
-    await QRCode.toFile(qrCodePath, qrData);
+  await QRCode.toFile(qrCodePath, qrData);
 
-    if (qrCodePath) {
-        const qrBuffer = fs.readFileSync(qrCodePath);
-        const q64 = qrBuffer.toString("base64");
-        formData.qrcodedata = q64;
-    }
+  if (qrCodePath) {
+    const qrBuffer = fs.readFileSync(qrCodePath);
+    const q64 = qrBuffer.toString("base64");
+    formData.qrcodedata = q64;
+  }
 
-    if (category === "boys")
-        await db.insert(boys).values({
-            name: formData.name,
-            email: formData.email,
-            mobileNo: formData.mobile_no,
-            usn: formData.usn,
-            uniqueCode: formData.unique_code,
-            qrcodedata: formData.qrcodedata
-        })
-    else if (category === "girls")
-        await db.insert(girls).values({
-            name: formData.name,
-            email: formData.email,
-            mobileNo: formData.mobile_no,
-            usn: formData.usn,
-            uniqueCode: formData.unique_code,
-            qrcodedata: formData.qrcodedata
-        })
-    else if (category === "walkathon")
-        await db.insert(walkathon).values({
-            name: formData.name,
-            email: formData.email,
-            mobileNo: formData.mobile_no,
-            uniqueCode: formData.unique_code,
-            qrcodedata: formData.qrcodedata
-        })
+  if (category === "boys")
+    await db.insert(boys).values({
+      name: formData.name,
+      email: formData.email,
+      mobileNo: formData.mobile_no,
+      usn: formData.usn,
+      uniqueCode: formData.unique_code,
+      qrcodedata: formData.qrcodedata,
+    });
+  else if (category === "girls")
+    await db.insert(girls).values({
+      name: formData.name,
+      email: formData.email,
+      mobileNo: formData.mobile_no,
+      usn: formData.usn,
+      uniqueCode: formData.unique_code,
+      qrcodedata: formData.qrcodedata,
+    });
+  else if (category === "walkathon")
+    await db.insert(walkathon).values({
+      name: formData.name,
+      email: formData.email,
+      mobileNo: formData.mobile_no,
+      uniqueCode: formData.unique_code,
+      qrcodedata: formData.qrcodedata,
+    });
 
-    await db.insert(master).values({
-        name: formData.name,
-        email: formData.email,
-        mobileNo: formData.mobile_no,
-        usn: formData.usn,
-        uniqueCode: formData.unique_code,
-        qrcodedata: formData.qrcodedata
-    })
+  await db.insert(master).values({
+    name: formData.name,
+    email: formData.email,
+    mobileNo: formData.mobile_no,
+    usn: formData.usn,
+    uniqueCode: formData.unique_code,
+    qrcodedata: formData.qrcodedata,
+  });
 
-    if (formData.usn &&
-        (formData.usn[1] == 'S' || formData.usn[1] == 's') &&
-        (formData.usn[2] == 'i' || formData.usn[2] == 'I')
-    ) {
-        await db.insert(sit).values({
-            name: formData.name,
-            email: formData.email,
-            mobileNo: formData.mobile_no,
-            usn: formData.usn,
-            uniqueCode: formData.unique_code,
-            qrcodedata: formData.qrcodedata
-        })
-    }
+  if (
+    formData.usn &&
+    (formData.usn[1] == "S" || formData.usn[1] == "s") &&
+    (formData.usn[2] == "i" || formData.usn[2] == "I")
+  ) {
+    await db.insert(sit).values({
+      name: formData.name,
+      email: formData.email,
+      mobileNo: formData.mobile_no,
+      usn: formData.usn,
+      uniqueCode: formData.unique_code,
+      qrcodedata: formData.qrcodedata,
+    });
+  }
 
-    transporter.sendMail(mailOpts, () => {
-        fs.unlinkSync(qrCodePath);
-    })
+  transporter.sendMail(mailOpts, () => {
+    fs.unlinkSync(qrCodePath);
+  });
 }
