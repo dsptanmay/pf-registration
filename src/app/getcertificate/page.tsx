adcc48f277b0a6c3bd42efd5e2d361a393d2db28
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-13 16:24:40 +0530

    added page to download certificates later

---
+++ src/app/getcertificate/page.tsx
@@ -0,0 +1,8 @@
+import GetCertificateComponent from "@/components/getCertificateComponent";
+import React from "react";
+
+const GetCertificatePage = () => {
+  return <GetCertificateComponent />;
+};
+
+export default GetCertificatePage;
