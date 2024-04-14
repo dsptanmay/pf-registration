5ce16bad143e2797860751d261f11aca66368cab
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-14 22:57:00 +0530

    added dotenv to hopefully fix env variables

--- src/actions/mailActions.ts
+++ src/actions/mailActions.ts
@@ -1,6 +1,11 @@
 import nodemailer from "nodemailer";
 import Mail from "nodemailer/lib/mailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.local",
+});
 
 export const primary_transporter = nodemailer.createTransport({
   service: "Gmail",
