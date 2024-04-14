4e416c26b492f3150d036b9bb78c09b2c1176947
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-14 20:12:14 +0530

    fixed issue of not being able to read env variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,15 @@
 import type { Config } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.local",
+});
 
 export default {
   schema: "src/database/schema.ts",
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    connectionString: process.env.NEXT_PUBLIC_DB_URL || "",
+    connectionString: process.env.NEXT_PUBLIC_DB_URL as string,
   },
 } satisfies Config;
