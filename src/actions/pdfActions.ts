8ea1294f17a3b0f00e502f7005cabe0b06d22fb0
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-20 20:54:40 +0530

    Added functionality to fetch sit participants pdf

--- src/actions/pdfActions.ts
+++ src/actions/pdfActions.ts
@@ -1,5 +1,5 @@
 import { PDFDocument, PageSizes, rgb, StandardFonts } from "pdf-lib";
-import { CrossData } from "@/types/forms";
+import { CrossData, SITData } from "@/types/forms";
 
 function fixTimeFormat(time: Date) {
   const timeZoneOffset = time.getTimezoneOffset();
@@ -20,12 +20,130 @@ function fixTimeFormat(time: Date) {
   return fixedTime;
 }
 
+export async function generateSITPDF(data: SITData[]) {
+  const pdfDoc = await PDFDocument.create();
+  const helveticaBoldFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+  const page = pdfDoc.addPage(PageSizes.A2);
+  const { width, height } = page.getSize();
+  const fontSize = 12;
+  const margin = 50;
+  const tableWidth = width - 2 * margin;
+
+  let y = height - margin;
+
+  const rowHeight = 25;
+  const cellPadding = 5;
+
+  const table = {
+    x: margin,
+    y,
+    rows: data.length + 1,
+    cols: 5,
+    rowHeight,
+    cellPadding,
+    tableWidth,
+    drawHorizontalLine: (x: number, y: number, length: number) => {
+      page.drawLine({
+        start: { x, y },
+        end: { x: x + length, y },
+        thickness: 1,
+        color: rgb(0, 0, 0),
+      });
+    },
+    drawVerticalLine: (x: number, y: number, length: number) => {
+      page.drawLine({
+        start: { x, y },
+        end: { x, y: y - length },
+        thickness: 1,
+        color: rgb(0, 0, 0),
+      });
+    },
+    writeText: (text: string, x: number, y: number) => {
+      page.drawText(text, {
+        x,
+        y: y - fontSize + 5,
+        size: fontSize,
+        color: rgb(0, 0, 0),
+      });
+    },
+  };
+
+  page.setFont(helveticaBoldFont);
+  page.drawText("SIT Participants", {
+    x: table.x,
+    y: table.y,
+    color: rgb(0, 0, 0),
+    size: 30,
+    opacity: 1,
+  });
+
+  table.y -= rowHeight + 10;
+  table.writeText("Name", table.x + table.cellPadding, table.y);
+  table.writeText(
+    "Unique Code",
+    table.x + table.cellPadding + tableWidth / 5,
+    table.y
+  );
+  table.writeText(
+    "USN",
+    table.x + table.cellPadding + (tableWidth * 2) / 5,
+    table.y
+  );
+  table.writeText(
+    "Email Address",
+    table.x + table.cellPadding + (tableWidth * 3) / 5,
+    table.y
+  );
+  table.writeText(
+    "Mobile Number",
+    table.x + table.cellPadding + (tableWidth * 4) / 5,
+    table.y
+  );
+
+  table.y -= rowHeight + 10;
+  page.setFont(helveticaFont);
+
+  data.forEach((participant) => {
+    table.writeText(participant.name, table.x + table.cellPadding, table.y);
+    table.writeText(
+      participant.uniqueCode,
+      table.x + table.cellPadding + tableWidth / 5,
+      table.y
+    );
+    table.writeText(
+      participant.usn,
+      table.x + table.cellPadding + (tableWidth * 2) / 5,
+      table.y
+    );
+    table.writeText(
+      participant.email as string,
+      table.x + table.cellPadding + (tableWidth * 3) / 5,
+      table.y
+    );
+    table.writeText(
+      participant.phone as string,
+      table.x + table.cellPadding + (tableWidth * 4) / 5,
+      table.y
+    );
+
+    table.y -= rowHeight;
+  });
+
+  for (let i = 0; i < table.rows; ++i) {
+    const y = table.y + 8 + table.rowHeight * i;
+    table.drawHorizontalLine(table.x, y, table.tableWidth);
+  }
+
+  const pdfBytes = await pdfDoc.save();
+  return pdfBytes;
+}
+
 export async function generatePdf(data: CrossData[], title?: string) {
   const pdfDoc = await PDFDocument.create();
-  const timesRomanBoldFont = await pdfDoc.embedFont(
-    StandardFonts.HelveticaBold
-  );
-  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+  const helveticaBoldFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
   const page = pdfDoc.addPage(PageSizes.A2);
   const { width, height } = page.getSize();
@@ -70,7 +188,7 @@ export async function generatePdf(data: CrossData[], title?: string) {
     },
   };
 
-  page.setFont(timesRomanBoldFont);
+  page.setFont(helveticaBoldFont);
   page.drawText(title as string, {
     x: table.x,
     y: table.y,
@@ -103,7 +221,7 @@ export async function generatePdf(data: CrossData[], title?: string) {
 
   table.y -= rowHeight + 10;
 
-  page.setFont(timesRomanFont);
+  page.setFont(helveticaFont);
   data.forEach((participant, idx) => {
     const formattedTime = fixTimeFormat(participant.timeCrossed);
     table.writeText(participant.name, table.x + table.cellPadding, table.y);
