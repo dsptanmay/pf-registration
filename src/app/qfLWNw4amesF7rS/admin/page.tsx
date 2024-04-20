d1822e1060974dac083d37a024ad48ac9da192e1
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-20 21:34:49 +0530

    added sit participants section

--- src/app/qfLWNw4amesF7rS/admin/page.tsx
+++ src/app/qfLWNw4amesF7rS/admin/page.tsx
@@ -1,16 +1,21 @@
 "use client";
 import BoysCrossComponent from "@/components/BoysTop20";
 import GirlsCrossComponent from "@/components/GirlsTop20";
+import SITParticipants from "@/components/SITParticipants";
 import WalkCrossComponent from "@/components/WalkathonTop10";
 import React from "react";
 
 const AdminPage = () => {
   return (
-    <div className="h-screen bg-gradient-to-br from-orange-500/40 via-blue-300 to-purple-300 flex flex-col justify-center">
-      <h1 className="font-bold text-4xl text-center mb-5 text-gray-900"> Admin Page</h1>
+    <div className=" bg-gradient-to-br from-orange-500/40 via-blue-300 to-purple-300 flex flex-col h-screen justify-center p-3">
+      <h1 className="font-bold text-4xl text-center mb-5 text-gray-900">
+        {" "}
+        Admin Page
+      </h1>
       <BoysCrossComponent />
       <GirlsCrossComponent />
       <WalkCrossComponent />
+      <SITParticipants />
     </div>
   );
 };
