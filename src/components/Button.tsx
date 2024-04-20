abf2f4334e8f3940f9668db6a8736eceb60f2d35
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-20 19:28:40 +0530

    testing out a new style of buttons

---
+++ src/components/Button.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export const MainButton = (props: { text: string; path: string }) => {
+  const router = useRouter();
+  return (
+    <button
+      className="bg-black rounded-lg"
+      onClick={() => {
+        router.push(props.path);
+      }}
+    >
+      <span
+        className="bg-[#f2994a] block p-4 -translate-y-1 border-black border-2 rounded-lg text-2xl
+      hover:-translate-y-2 active:translate-x-0 active:translate-y-0 transition-all"
+      >
+        {props.text}
+      </span>
+    </button>
+  );
+};
