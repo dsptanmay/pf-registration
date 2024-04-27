b6087ec5a5ae5275613825e2d7c200c94bee4514
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-27 18:30:17 +0530

    removed draw vertical line and made table a separate entity to be called repeatedly

--- src/actions/pdfActions.ts
+++ src/actions/pdfActions.ts
@@ -1,4 +1,4 @@
-import { PDFDocument, PageSizes, rgb, StandardFonts } from "pdf-lib";
+import { PDFDocument, PageSizes, rgb, StandardFonts, PDFPage } from "pdf-lib";
 import { CrossData, SITData } from "@/types/forms";
 
 function fixTimeFormat(time: Date) {
@@ -20,31 +20,31 @@ function fixTimeFormat(time: Date) {
   return fixedTime;
 }
 
-export async function generateSITPDF(data: SITData[]) {
+export async function generateSITPDF(data: SITData[][]) {
   const pdfDoc = await PDFDocument.create();
   const helveticaBoldFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
   const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
 
-  const page = pdfDoc.addPage(PageSizes.A2);
-  const { width, height } = page.getSize();
+  const pageWidth = PageSizes.A2[0];
+  const pageHeight = PageSizes.A2[1];
+
   const fontSize = 12;
   const margin = 50;
-  const tableWidth = width - 2 * margin;
-
-  let y = height - margin;
-
   const rowHeight = 25;
   const cellPadding = 5;
 
   const table = {
-    x: margin,
-    y,
-    rows: data.length + 1,
+    rows: 0,
     cols: 5,
     rowHeight,
     cellPadding,
-    tableWidth,
-    drawHorizontalLine: (x: number, y: number, length: number) => {
+
+    drawHorizontalLine: (
+      page: PDFPage,
+      x: number,
+      y: number,
+      length: number
+    ) => {
       page.drawLine({
         start: { x, y },
         end: { x: x + length, y },
@@ -52,15 +52,8 @@ export async function generateSITPDF(data: SITData[]) {
         color: rgb(0, 0, 0),
       });
     },
-    drawVerticalLine: (x: number, y: number, length: number) => {
-      page.drawLine({
-        start: { x, y },
-        end: { x, y: y - length },
-        thickness: 1,
-        color: rgb(0, 0, 0),
-      });
-    },
-    writeText: (text: string, x: number, y: number) => {
+
+    writeText: (page: PDFPage, text: string, x: number, y: number) => {
       page.drawText(text, {
         x,
         y: y - fontSize + 5,
@@ -70,70 +63,87 @@ export async function generateSITPDF(data: SITData[]) {
     },
   };
 
-  page.setFont(helveticaBoldFont);
-  page.drawText("SIT Participants", {
-    x: table.x,
-    y: table.y,
-    color: rgb(0, 0, 0),
-    size: 30,
-    opacity: 1,
-  });
+  for (const chunk of data) {
+    const page = pdfDoc.addPage([pageWidth, pageHeight]);
+    let y = pageHeight - margin;
+    const tableWidth = pageWidth - 2 * margin;
 
-  table.y -= rowHeight + 10;
-  table.writeText("Name", table.x + table.cellPadding, table.y);
-  table.writeText(
-    "Unique Code",
-    table.x + table.cellPadding + tableWidth / 5,
-    table.y
-  );
-  table.writeText(
-    "USN",
-    table.x + table.cellPadding + (tableWidth * 2) / 5,
-    table.y
-  );
-  table.writeText(
-    "Email Address",
-    table.x + table.cellPadding + (tableWidth * 3) / 5,
-    table.y
-  );
-  table.writeText(
-    "Mobile Number",
-    table.x + table.cellPadding + (tableWidth * 4) / 5,
-    table.y
-  );
+    table.rows = Math.min(chunk.length, 57) + 1;
 
-  table.y -= rowHeight + 10;
-  page.setFont(helveticaFont);
+    page.setFont(helveticaBoldFont);
 
-  data.forEach((participant) => {
-    table.writeText(participant.name, table.x + table.cellPadding, table.y);
+    page.drawText("SIT Participants", {
+      x: margin,
+      y,
+      color: rgb(0, 0, 0),
+      size: 30,
+      opacity: 1,
+    });
+
+    y -= rowHeight + 10;
+    table.writeText(page, "Name", margin + table.cellPadding, y);
     table.writeText(
-      participant.uniqueCode,
-      table.x + table.cellPadding + tableWidth / 5,
-      table.y
+      page,
+      "Unique Code",
+      margin + table.cellPadding + tableWidth / 5,
+      y
     );
     table.writeText(
-      participant.usn,
-      table.x + table.cellPadding + (tableWidth * 2) / 5,
-      table.y
+      page,
+      "USN",
+      margin + table.cellPadding + (tableWidth * 2) / 5,
+      y
     );
     table.writeText(
-      participant.email as string,
-      table.x + table.cellPadding + (tableWidth * 3) / 5,
-      table.y
+      page,
+      "Email Address",
+      margin + table.cellPadding + (tableWidth * 3) / 5,
+      y
     );
     table.writeText(
-      participant.phone as string,
-      table.x + table.cellPadding + (tableWidth * 4) / 5,
-      table.y
+      page,
+      "Mobile Number",
+      margin + table.cellPadding + (tableWidth * 4) / 5,
+      y
     );
 
-    table.y -= rowHeight;
-  });
+    y -= rowHeight + 10;
+    page.setFont(helveticaFont);
 
-  for (let i = 0; i < table.rows; ++i) {
-    const y = table.y + 8 + table.rowHeight * i;
-    table.drawHorizontalLine(table.x, y, table.tableWidth);
+    for (const participant of chunk) {
+      table.writeText(page, participant.name, margin + table.cellPadding, y);
+      table.writeText(
+        page,
+        participant.uniqueCode,
+        margin + table.cellPadding + tableWidth / 5,
+        y
+      );
+      table.writeText(
+        page,
+        participant.usn,
+        margin + table.cellPadding + (tableWidth * 2) / 5,
+        y
+      );
+      table.writeText(
+        page,
+        participant.email,
+        margin + table.cellPadding + (tableWidth * 3) / 5,
+        y
+      );
+      table.writeText(
+        page,
+        participant.phone,
+        margin + table.cellPadding + (tableWidth * 4) / 5,
+        y
+      );
+
+      y -= rowHeight;
+    }
+
+    for (let i = 0; i < table.rows; ++i) {
+      const lineY = y + 8 + table.rowHeight * i;
+      table.drawHorizontalLine(page, margin, lineY, tableWidth);
+    }
   }
 
   const pdfBytes = await pdfDoc.save();
@@ -150,9 +160,23 @@ export async function generatePdf(data: CrossData[], title?: string) {
   const fontSize = 12;
   const margin = 50;
   const tableWidth = width - 2 * margin;
-  let y = height - margin;
   const rowHeight = 25;
   const cellPadding = 5;
+
+  const logoUrl = "https://i.postimg.cc/x8Jwt2dT/pf-logo-vector-5k.png";
+  const logoBytes = await fetch(logoUrl).then((img) => img.arrayBuffer());
+
+  const pngImage = await pdfDoc.embedPng(logoBytes);
+  const pngDims = pngImage.scale(0.3);
+
+  page.drawImage(pngImage, {
+    x: width / 2 - pngDims.width,
+    y: height - margin - pngDims.height,
+    width: pngDims.width,
+    height: pngDims.height,
+  });
+
+  let y = height - margin * 2 - pngDims.height;
 
   const table = {
     x: margin,
