0f010848770abdf74d5517652457d8b45f6a9eeb
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-28 21:58:16 +0530

    Added function to generate PDFs for certificate

--- src/components/getCertificateComponent.tsx
+++ src/components/getCertificateComponent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { sendCertificateEmail } from "@/actions/certActions";
+import { generateCertificate } from "@/actions/certActions";
 import { getOneParticipant } from "@/actions/dataActions";
 
 import { toastOpts } from "@/utils/freq";
@@ -11,26 +11,39 @@ import * as z from "zod";
 import pfLogo from "../../public/pf-white.png";
 import Image from "next/image";
 
-const schema = z.string().email("Invalid Email Address");
+const schema = z.string().regex(/^\d{5}[A-Z]$/);
 
 const GetCertificateComponent = () => {
-  const [email, setEmail] = useState("");
+  const [uniqueCode, setUniqueCode] = useState("");
   const [error, setErrors] = useState("");
+  const [loading, setLoading] = useState<boolean>(false);
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
-      schema.parse(email);
-      const res = await getOneParticipant(email);
-      if (res.length === 0) toast.error("Could not find email!", toastOpts);
+      const res = await getOneParticipant(uniqueCode);
+      if (res === undefined)
+        toast.error("Could not find participant!", toastOpts);
       else {
-        sendCertificateEmail(res[0].name as string, res[0].email as string);
-        toast.success(
-          "Mail sent to your email account successfully!",
-          toastOpts
-        );
+        setLoading(true);
+        const pdfBytes = await generateCertificate(res.name as string);
+        const blob = new Blob([pdfBytes], { type: "application/pdf" });
+
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+
+        document.appendChild(a);
+
+        a.href = url;
+        a.download = "CertificateOfParticipation.pdf";
+        a.click();
+
+        toast.success("Certificate downloaded successfully!", toastOpts);
+        window.URL.revokeObjectURL(url);
+        document.removeChild(a);
+        setLoading(false);
       }
-      setEmail("");
+      setUniqueCode("");
       setErrors("");
     } catch (err) {
       if (err instanceof z.ZodError) {
@@ -50,17 +63,18 @@ const GetCertificateComponent = () => {
       />
       <form onSubmit={handleSubmit} className="flex flex-col items-center">
         <input
-          type="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your registered email id"
+          type="text"
+          name="unique_code"
+          value={uniqueCode}
+          onChange={(e) => setUniqueCode(e.target.value)}
+          placeholder="Enter your Unique Code"
           className="mb-4 px-10 py-5 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 drop-shadow-md"
         />
         {error && <p className="mb-4 text-red-500">{error}</p>}
         <button type="submit" className="bg-black rounded-lg">
           <span className="block p-4 border-black border-2 -translate-y-1 bg-orange-400 rounded-lg text-white text-xl font-semibold hover:-translate-y-2 transition-all active:translate-x-0 active:translate-y-0">
-            Submit
+            {!loading && "Submit"}
+            {loading && "Loading"}
           </span>
         </button>
       </form>
