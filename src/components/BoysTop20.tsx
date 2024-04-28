eaad0c898b55d28757bfbbe7c7dd1f953c71bba4
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-04-28 23:42:39 +0530

    moved generate pdf functions to client side along with time formatting, to ensure faster generation

--- src/components/BoysTop20.tsx
+++ src/components/BoysTop20.tsx
@@ -1,11 +1,176 @@
 "use client";
 import { getTopParticipants } from "@/actions/dataActions";
 import { CrossData } from "@/types/forms";
-import React, { useState } from "react";
-import { generatePdf } from "@/actions/pdfActions";
+import React, { useEffect, useState } from "react";
+import { PDFDocument, PageSizes, StandardFonts, rgb } from "pdf-lib";
 
 const BoysCrossComponent: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(false);
+  const [logoBytes, setLogoBytes] = useState<ArrayBuffer>();
+
+  useEffect(() => {
+    const fetchLogo = async () => {
+      const logoBuffer = await fetch(
+        process.env.NEXT_PUBLIC_PF_URL as string
+      ).then((img) => img.arrayBuffer());
+      setLogoBytes(logoBuffer);
+    };
+
+    fetchLogo();
+  }, []);
+
+  function fixTimeFormat(time: Date) {
+    const timeZoneOffset = time.getTimezoneOffset();
+    const adjustedTime = new Date(time.getTime() + timeZoneOffset * 60 * 1000);
+
+    const month = String(adjustedTime.getMonth() + 1).padStart(2, "0");
+    const date = String(adjustedTime.getDate()).padStart(2, "0");
+
+    const formattedDate = `${date}-${month}-${adjustedTime.getFullYear()}`;
+
+    let hours = adjustedTime.getHours();
+    const minutes = String(adjustedTime.getMinutes()).padStart(2, "0");
+    const seconds = String(adjustedTime.getSeconds()).padStart(2, "0");
+
+    const amPm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    const fixedTime = `${formattedDate} ${hours}:${minutes}:${seconds} ${amPm}`;
+    return fixedTime;
+  }
+
+  async function generatePdf(data: CrossData[], title?: string) {
+    const pdfDoc = await PDFDocument.create();
+    const helveticaBoldFont = await pdfDoc.embedFont(
+      StandardFonts.HelveticaBold
+    );
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+    const page = pdfDoc.addPage(PageSizes.A2);
+    const { width, height } = page.getSize();
+    const fontSize = 12;
+    const margin = 50;
+    const tableWidth = width - 2 * margin;
+    const rowHeight = 25;
+    const cellPadding = 5;
+
+    const logoUrl = "https://i.postimg.cc/x8Jwt2dT/pf-logo-vector-5k.png";
+    const logoBytes = await fetch(logoUrl).then((img) => img.arrayBuffer());
+
+    const pngImage = await pdfDoc.embedPng(logoBytes);
+    const pngDims = pngImage.scale(0.1);
+
+    page.drawImage(pngImage, {
+      x: width - margin - pngDims.width,
+      y: height - margin + 10 - pngDims.height,
+      width: pngDims.width,
+      height: pngDims.height,
+    });
+
+    let y = height - margin * 2;
+
+    const table = {
+      x: margin,
+      y,
+      rows: data.length + 1,
+      cols: 5,
+      rowHeight,
+      cellPadding,
+      tableWidth,
+      drawHorizontalLine: (x: number, y: number, length: number) => {
+        page.drawLine({
+          start: { x, y },
+          end: { x: x + length, y },
+          thickness: 1,
+          color: rgb(0, 0, 0),
+        });
+      },
+      drawVerticalLine: (x: number, y: number, length: number) => {
+        page.drawLine({
+          start: { x, y },
+          end: { x, y: y - length },
+          thickness: 1,
+          color: rgb(0, 0, 0),
+        });
+      },
+      writeText: (text: string, x: number, y: number) => {
+        page.drawText(text, {
+          x,
+          y: y - fontSize + 5,
+          size: fontSize,
+          color: rgb(0, 0, 0),
+        });
+      },
+    };
+
+    page.setFont(helveticaBoldFont);
+    page.drawText(title as string, {
+      x: table.x,
+      y: table.y,
+      color: rgb(0, 0, 0),
+      size: 30,
+      opacity: 1,
+    });
+    table.y -= rowHeight + 10;
+    table.writeText("Name", table.x + table.cellPadding, table.y);
+    table.writeText(
+      "Unique Code",
+      table.x + table.cellPadding + tableWidth / 5,
+      table.y
+    );
+    table.writeText(
+      "Time Crossed",
+      table.x + table.cellPadding + (tableWidth * 2) / 5,
+      table.y
+    );
+    table.writeText(
+      "Email",
+      table.x + table.cellPadding + (tableWidth * 3) / 5,
+      table.y
+    );
+    table.writeText(
+      "Mobile",
+      table.x + table.cellPadding + (tableWidth * 4) / 5,
+      table.y
+    );
+
+    table.y -= rowHeight + 10;
+
+    page.setFont(helveticaFont);
+    data.forEach((participant, idx) => {
+      const formattedTime = fixTimeFormat(participant.timeCrossed);
+      table.writeText(participant.name, table.x + table.cellPadding, table.y);
+      table.writeText(
+        participant.uniqueCode,
+        table.x + table.cellPadding + tableWidth / 5,
+        table.y
+      );
+      table.writeText(
+        formattedTime,
+        table.x + table.cellPadding + (tableWidth * 2) / 5,
+        table.y
+      );
+      table.writeText(
+        participant.email || "",
+        table.x + table.cellPadding + (tableWidth * 3) / 5,
+        table.y
+      );
+      table.writeText(
+        participant.phone || "",
+        table.x + table.cellPadding + (tableWidth * 4) / 5,
+        table.y
+      );
+
+      table.y -= rowHeight;
+    });
+
+    for (let i = 0; i < table.rows; ++i) {
+      const y = table.y + 8 + table.rowHeight * i;
+      table.drawHorizontalLine(table.x, y, table.tableWidth);
+    }
+
+    const pdfBytes = await pdfDoc.save();
+    return pdfBytes;
+  }
 
   const handleDownload = async (params: CrossData[]) => {
     const pdfBytes = await generatePdf(params, "Top 20 Participants - Boys");
@@ -24,9 +189,9 @@ const BoysCrossComponent: React.FC = () => {
 
   const handleSubmit = async () => {
     setLoading(true);
-    const res2 = await getTopParticipants("boys");
-    console.log(res2);
-    await handleDownload(res2 as CrossData[]);
+    const res = await getTopParticipants("boys");
+    console.log(res);
+    await handleDownload(res as CrossData[]);
     setLoading(false);
   };
 
