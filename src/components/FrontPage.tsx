71c58c8aa95104f073222bd25edc79f8cfd362d2
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-05-03 17:59:49 +0530

    changed design to be responsive

--- src/components/FrontPage.tsx
+++ src/components/FrontPage.tsx
@@ -1,23 +1,33 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
-import pfLogo from "../../public/pf-white.png"
+import pfLogo from "../../public/pf-white.png";
+import avLogo from "../../public/Aryavartha.jpg";
 import Image from "next/image";
 import { MainButton } from "@/components/Button";
 
 const FrontPage: React.FC = () => {
-    const router = useRouter();
-    return (
-        <div className="h-screen max:h-screen-auto flex flex-col justify-center items-center bg-gradient-to-br from-orange-500/35 via-blue-300 to-purple-400/40">
-            <Image src={pfLogo} alt="Pathfinder Logo" height={150} className="absolute top-[0rem] m-3"></Image>
-            <div className="bg-[#f7f7f7] rounded-lg drop-shadow-lg flex flex-col justify-between p-10 w-11/12 h-auto gap-4 ">
-                <h2 className="text-center text-3xl mb-3 font-bold">Registrations</h2>
-                <MainButton text="Boys Marathon" path="/boys"/>
-                <MainButton text="Girls Marathon" path="/girls"/>
-                <MainButton text="Walkathon" path="/walkathon"/>
-            </div>
-        </div>
-    );
-}
+  const router = useRouter();
+  return (
+    <div className="max:h-screen-auto flex flex-col justify-center items-center bg-gradient-to-br gap-4 from-orange-500/35 via-blue-300 to-purple-400/40 pb-10 md:pb-28">
+      <Image
+        src={pfLogo}
+        alt="Pathfinder Logo"
+        height={150}
+        className="m-2 p-2"
+      ></Image>
+      <div className="flex flex-wrap gap-2 w-full justify-around items-center ">
+        <Image src={avLogo} alt="Aryavartha Logo" height={100} className="mix-blend-multiply "></Image>
+        <p className="text-xl md:text-4xl text-white drop-shadow-md text-center">"Smart Solutions for a Sustainable Future"</p>
+      </div>
+      <div className="bg-[#f7f7f7] rounded-lg drop-shadow-lg flex flex-col justify-between p-10 gap-4 ">
+        <h2 className="text-center text-3xl mb-3 font-bold">Registrations</h2>
+        <MainButton text="Boys Marathon" path="/boys" />
+        <MainButton text="Girls Marathon" path="/girls" />
+        <MainButton text="Walkathon" path="/walkathon" />
+      </div>
+    </div>
+  );
+};
 
 export default FrontPage;
